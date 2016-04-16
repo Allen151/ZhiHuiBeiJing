f4c7b591c9bcf6bd2927cc0c8283cec78d48916e
xiphodon <827789895@qq.com>
2016-04-17 00:36:37 +0800

    智慧北京（已完成闪屏页+引导页； 第三方SlidingMenu库，初始化侧边栏和显示的主页； 第三方xUtils库，主页下五个子页面的实现； 页签切换优化； 框架结构已经搭建完成，采用gson解析json； 第三方ViewPagerIndicator库，滑动事件处理； 完成从服务器拉取数据，下拉刷新布局； 完成新闻详情页，下拉刷新，上拉加载下一页； 完成新闻详情页返回、修改字体大小功能，WebView各个小功能；）

--- app/src/main/java/com/gc/zhbj/activity/NewsDetailActivity.java
+++ app/src/main/java/com/gc/zhbj/activity/NewsDetailActivity.java
@@ -1,11 +1,15 @@
 package com.gc.zhbj.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,7 +22,7 @@ import com.gc.zhbj.R;
 /**
  * 新闻详情页
  */
-public class NewsDetailActivity extends Activity {
+public class NewsDetailActivity extends Activity implements View.OnClickListener {
 
     private WebView wv_web;
     private ImageButton btn_back;
@@ -40,6 +44,10 @@ public class NewsDetailActivity extends Activity {
         btn_share = (ImageButton) findViewById(R.id.btn_share);
         pb_progress = (ProgressBar) findViewById(R.id.pb_progress);
 
+        btn_back.setOnClickListener(this);
+        btn_size.setOnClickListener(this);
+        btn_share.setOnClickListener(this);
+
         String url = getIntent().getStringExtra("url");
 
         WebSettings settings = wv_web.getSettings();
@@ -58,7 +66,7 @@ public class NewsDetailActivity extends Activity {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
-                System.out.println("网页开始加载");
+                Log.i("i", "网页开始加载");
                 pb_progress.setVisibility(View.VISIBLE);
             }
 
@@ -68,7 +76,7 @@ public class NewsDetailActivity extends Activity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                System.out.println("网页加载结束");
+                Log.i("i", "网页加载结束");
                 pb_progress.setVisibility(View.GONE);
             }
 
@@ -78,11 +86,32 @@ public class NewsDetailActivity extends Activity {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 // 可以在每次跳转时拦截做自定义处理
-                System.out.println("跳转url:" + url);
+                Log.i("i", "跳转url" + url);
                 view.loadUrl(url);
 
                 return true;
                 // return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        wv_web.setWebChromeClient(new WebChromeClient() {
+
+            /**
+             * 进度发生变化
+             */
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                Log.i("i", "加载进度:" + newProgress);
+                super.onProgressChanged(view, newProgress);
+            }
+
+            /**
+             * 获取网页标题
+             */
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                Log.i("i", "网页标题:" + title);
+                super.onReceivedTitle(view, title);
             }
         });
 //        上一页、下一页
@@ -90,5 +119,92 @@ public class NewsDetailActivity extends Activity {
 //        wv_web.goForward();
 
         wv_web.loadUrl(url);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_back:
+                finish();
+                break;
+            case R.id.btn_size:
+                showChooseDialog();
+                break;
+            case R.id.btn_share:
+                showShare();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // 记录当前选中的item, 点击确定前临时记录
+    private int mCurrentChooseItem;
+    // 记录当前选中的item, 点击确定后记录
+    private int mCurrentItem = 2;
+
+    /**
+     * 显示选择对话框
+     */
+    private void showChooseDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        String[] items = new String[]{"超大号字体", "大号字体", "正常字体", "小号字体", "超小号字体"};
+        builder.setTitle("字体设置");
+        builder.setSingleChoiceItems(items, mCurrentItem, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i("i", "选中:" + which);
+                mCurrentChooseItem = which;
+            }
+        });
+
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                WebSettings settings = wv_web.getSettings();
+                switch (mCurrentChooseItem) {
+                    case 0:
+//                      settings.setTextSize(WebSettings.TextSize.LARGEST);
+                        settings.setTextZoom(200);
+                        break;
+                    case 1:
+//                      settings.setTextSize(WebSettings.TextSize.LARGER);
+                        settings.setTextZoom(150);
+                        break;
+                    case 2:
+//                      settings.setTextSize(WebSettings.TextSize.NORMAL);
+                        settings.setTextZoom(100);
+                        break;
+                    case 3:
+//                      settings.setTextSize(WebSettings.TextSize.SMALLER);
+                        settings.setTextZoom(75);
+                        break;
+                    case 4:
+//                      settings.setTextSize(WebSettings.TextSize.SMALLEST);
+                        settings.setTextZoom(50);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                mCurrentItem = mCurrentChooseItem;
+            }
+        });
+
+        builder.setNegativeButton("取消", null);
+
+        builder.show();
+    }
+
+    /**
+     * 分享, 注意在sdcard根目录放test.jpg
+     */
+    private void showShare() {
+
     }
 }
